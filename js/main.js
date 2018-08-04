f2d6bd3e90a1ac1af0ae6e435270dff516d065b0
danielwang <danielwang130@gmail.com>
2018-08-04 22:21:17 +1000

    add close icon on menu

--- js/main.js
+++ js/main.js
@@ -15,45 +15,10 @@
         // Mobile menu
         $('.menu-toggle').click(function() {
             $('.site-nav').toggleClass('mobile-menu-hide');
+            $('#hamburger, #close').toggleClass('d-none');
         });
         $('[data-toggle="tooltip"]').tooltip();
 
-        // // Testimonials Slider
-        // $(".testimonials.owl-carousel").owlCarousel({
-        //     nav: true, // Show next/prev buttons.
-        //     items: 1, // The number of items you want to see on the screen.
-        //     loop: true, // Infinity loop. Duplicate last and first items to get loop illusion.
-        //     navText: false,
-        //     margin: 10,
-        // });
-
-         // Blog grid init
-        // setTimeout(function(){
-        //     var $container = $(".blog-masonry");
-        //     $container.masonry();
-        // }, 500);
-        //
-        // $('.site-main-menu').on("click", "a", function (e) {
-        //     var $container = $(".blog-masonry");
-        //     $container.masonry();
-        // });
-
-
     });
-
-    // Animate page loader
-    // $(window).on('load', function() {
-    //     $(".preloader").fadeOut("slow");
-    // });
-    //
-    // // Mobile menu hide
-    // $(window).on('resize', function() {
-    //      mobileMenuHide();
-    // });
-    //
-    // // Mobile menu hide on main menu item click
-    // $('.site-main-menu').on("click", "a", function (e) {
-    //     mobileMenuHide();
-    // });
 
 })(jQuery);
